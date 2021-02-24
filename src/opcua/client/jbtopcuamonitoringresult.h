fe2676b86e1ece258801ff457a8381010da9f519
Bohdan Shamborskyi <bshamborskyi@kaaiot.io>
2021-02-24 11:08:57 +0200

    [OJI-4864] Added revised QOpcUaMonitoringParameters

--- src/opcua/client/jbtopcuamonitoringresult.h
+++ src/opcua/client/jbtopcuamonitoringresult.h
@@ -2,6 +2,7 @@
 #define JBTOPCUAMONITORINGRESULT_H
 
 #include <QtOpcUa/qopcuatype.h>
+#include <QtOpcUa/qopcuamonitoringparameters.h>
 #include <QtCore/qshareddata.h>
 
 QT_BEGIN_NAMESPACE
@@ -13,15 +14,21 @@ class Q_OPCUA_EXPORT JBTOpcUaMonitoringResult
 public:
     JBTOpcUaMonitoringResult();
     JBTOpcUaMonitoringResult(const JBTOpcUaMonitoringResult &other);
-    JBTOpcUaMonitoringResult(const QString &nodeId, const QMap<QOpcUa::NodeAttribute, QOpcUa::UaStatusCode> &monitorResults);
+    JBTOpcUaMonitoringResult(const QString &nodeId,
+                             const QMap<QOpcUa::NodeAttribute, QOpcUa::UaStatusCode> &monitoringResults, 
+                             const QOpcUaMonitoringParameters& monitoringParametersResult
+                            );
     JBTOpcUaMonitoringResult &operator=(const JBTOpcUaMonitoringResult &rhs);
     ~JBTOpcUaMonitoringResult();
 
     QString nodeId() const;
     void setNodeId(const QString &nodeId);
 
-    QMap<QOpcUa::NodeAttribute, QOpcUa::UaStatusCode> monitorResults() const;
-    void setMonitorResults(const QMap<QOpcUa::NodeAttribute, QOpcUa::UaStatusCode> &monitorResults);
+    QMap<QOpcUa::NodeAttribute, QOpcUa::UaStatusCode> monitoringResults() const;
+    void setMonitoringResults(const QMap<QOpcUa::NodeAttribute, QOpcUa::UaStatusCode>& monitoringResults);
+
+    QOpcUaMonitoringParameters monitoringParametersResult() const;
+    void setMonitoringParametersResult(const QOpcUaMonitoringParameters& monitoringParametersResult);
 
 private:
     QSharedDataPointer<JBTOpcUaMonitoringResultData> data;
